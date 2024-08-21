52d3237059ccbeea4c38ede999d25cbc45347c10
K-Shibin <aslua3567@gmail.com>
2024-08-21 11:10:28 +0530

    initial commit

---
+++ lib/homePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SqfLite extends StatelessWidget {
+  const SqfLite({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlueAccent[100],
+      ),
+    );
+  }
+}
