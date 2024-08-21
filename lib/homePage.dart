05df864aae5314ec38f6525a801c4a6befd753d3
K-Shibin <aslua3567@gmail.com>
2024-08-21 11:31:32 +0530

    new variable

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -2,7 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SqfLite extends StatelessWidget {
-  const SqfLite({super.key});
+   SqfLite({super.key});
+  int ?name;
+
+
 
   @override
   Widget build(BuildContext context) {
